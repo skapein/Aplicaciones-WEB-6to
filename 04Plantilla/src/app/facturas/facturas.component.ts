d5fa1b50a6b553ca8317cea39380c0478c54eda5
chrisMJ <chris666.10@hotmail.com>
2024-09-01 23:11:20 -0500

    actualizacion componente actualizar eliminar

--- 04Plantilla/src/app/facturas/facturas.component.ts
+++ 04Plantilla/src/app/facturas/facturas.component.ts
@@ -3,22 +3,62 @@ import { SharedModule } from 'src/app/theme/shared/shared.module';
 import { IFactura } from '../Interfaces/factura';
 import { Router, RouterLink } from '@angular/router';
 import { FacturaService } from '../Services/factura.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-facturas',
   standalone: true,
   imports: [SharedModule, RouterLink],
   templateUrl: './facturas.component.html',
-  styleUrl: './facturas.component.scss'
+  styleUrls: ['./facturas.component.scss']
 })
 export class FacturasComponent implements OnInit {
   listafacturas: IFactura[] = [];
-  constructor(private facturaServicio: FacturaService) {}
+
+  constructor(private facturaServicio: FacturaService, private router: Router) {}
+
   ngOnInit(): void {
-    this.facturaServicio.todos().subscribe((data: IFactura[]) => {
-      this.listafacturas = data;
+    this.cargarFacturas();
+  }
+
+  cargarFacturas(): void {
+    this.facturaServicio.todos().subscribe(
+      (data: IFactura[]) => {
+        this.listafacturas = data;
+      },
+      error => {
+        console.error('Error al cargar las facturas:', error);
+      }
+    );
+  }
+
+  eliminar(idFactura: number): void {
+    Swal.fire({
+      title: 'Eliminar Factura',
+      text: '¿Está seguro de eliminar esta factura?',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#f00',
+      cancelButtonColor: '#3085d6',
+      confirmButtonText: 'Eliminar'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.facturaServicio.eliminar(idFactura).subscribe(
+          () => {
+            Swal.fire('Factura eliminada', 'La factura ha sido eliminada correctamente.', 'success');
+            this.cargarFacturas(); // Refrescar la lista de facturas
+          },
+          error => {
+            console.error('Error al eliminar la factura:', error);
+            Swal.fire('Error', 'Hubo un problema al eliminar la factura.', 'error');
+          }
+        );
+      }
     });
   }
 
-  eliminar(idFactura) {}
+  actualizar(idFactura: number): void {
+   
+    this.router.navigate(['/facturas/editar', idFactura]);
+  }
 }
